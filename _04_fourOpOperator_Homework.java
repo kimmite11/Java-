8e77fddde7a7fc13db83a55ba241208d5eca7718
kimmite11 <124754326+kimmite11@users.noreply.github.com>
2023-02-13 03:04:07 +0900

    Add files via upload

---
+++ _04_fourOpOperator_Homework.java
@@ -0,0 +1,40 @@
+package chap03_operator;
+
+public class _04_fourOpOperator_Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // 1. 영문자를 입력받습니다
+		// 소문자인지 대문자인지 판별하세요 (조건 연산자를 사용하세요)
+
+    	System.out.println ("조건 연산자");
+		char ch = 'C';
+		String alphabet;
+		alphabet = (ch >='A' && ch <='Z') ? "대문자" : "소문자";
+		System.out.println("alphabet :" + alphabet);
+		System.out.println("ch :" + ch);
+	    System.out.println("-----------------------------");
+	    
+		// 2. 영문자를 입력받습니다
+		// 소문자는 대문자로, 대문자는 소문자로 변경해서 출력하세요 
+		// (조건 연산자를 사용하세요)
+		// (아스키테이블/유니코드테이블을 참조하면 됩니다)
+	}
+	  {char a,b;
+    	System.out.print("C");
+        a=(char) 
+		System.in.read():
+    	b=a>='a'&&a<'z'?(char)(a-32):
+        (a>='A'&&a<='Z'?(char)(a+32) : a);
+    	System.out.println(a+"->"+b);
+    	
+    	
+	  }
+		  
+}
+	
+        
+
+		
+	   	
+
